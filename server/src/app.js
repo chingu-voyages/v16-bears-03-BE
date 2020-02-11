77bf43f1f88b7a02313837dfe7adfe2d7e7826a1
Russ yev <ruslanyev@yahoo.com>
2020-02-10 23:18:06 -0800

    moved server into it's own directory

---
+++ server/src/app.js
@@ -0,0 +1,32 @@
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
+const { NODE_ENV } = require("./config");
+
+const app = express();
+
+const morganOption = NODE_ENV === "production" ? "tiny" : "common";
+
+app.use(morgan(morganOption));
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello, boilerplate!");
+});
+
+app.use(function errorHandler(error, req, res, next) {
+  let response;
+  if (NODE_ENV === "production") {
+    response = { error: { message: "server error" } };
+  } else {
+    console.error(error);
+    response = { message: error.message, error };
+  }
+  res.status(500).json(response);
+});
+
+module.exports = app;
