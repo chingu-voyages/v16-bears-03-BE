715418f46235d37714f9a097cd69e50fc7536dcd
Russ yev <ruslanyev@yahoo.com>
2020-02-10 23:20:56 -0800

    removed old files

--- src/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  PORT: process.env.PORT || 8000,
-  NODE_ENV: process.env.NODE_ENV || "development",
-  DATABASE_URL: process.env.DATABASE_URL,
-  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL
-};
