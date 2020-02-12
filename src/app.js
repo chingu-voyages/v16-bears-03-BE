f1f034ea49caa8f1853a5caa1ad3a65bed80cb1f
Igor Todorovic <igor333m@yahoo.com>
2020-02-11 17:24:07 -0800

    Add User schema and route

--- src/app.js
+++ src/app.js
@@ -4,8 +4,13 @@ const morgan = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
 const { NODE_ENV } = require("./config");
+const connectDB = require('./connectDB');
+
 
 const app = express();
+
+// Connect DB
+connectDB();
 
 const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 
@@ -13,9 +18,14 @@ app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
 
+app.use(express.json({extended: false}));
+
 app.get("/", (req, res) => {
   res.send("Hello, boilerplate!");
 });
+
+// Create user route
+app.use('/api/users', require('./routes/users'));
 
 app.use(function errorHandler(error, req, res, next) {
   let response;
