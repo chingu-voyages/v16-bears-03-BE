1cfe6cf92db158e99f005f7951f29bcc63d0ab77
JunjPan <elisepjj@hotmail.com>
2020-02-12 14:12:04 -0800

    deleted comment

--- src/server.js
+++ src/server.js
@@ -11,9 +11,8 @@ function runServer(databaseUrl, port = PORT) {
   return new Promise((resolve, reject) => {
     // mongoose.set('debug', true);
     mongoose.set("useCreateIndex", true);
-    //DeprecationWarning: current Server Discovery and Monitoring engine is deprecated, and will be removed in a future version. 
-    //To use the new Server Discover and Monitoring engine, pass option { useUnifiedTopology: true } to the MongoClient constructor.
-    mongoose.connect(databaseUrl, { useNewUrlParser: true, useUnifiedTopology: true  }, err => {
+    
+    mongoose.connect(databaseUrl, { useNewUrlParser: true}, err => {
       if (err) {
         return reject(err);
       }
