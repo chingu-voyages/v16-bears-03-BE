b30a69762278f895848c5ef074466f030c906a50
Igor Todorovic <igor333m@yahoo.com>
2020-02-12 17:54:45 -0800

    Add mongoose and express-validator

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
-const User = require('../../models/User');
+const User = require('../models/User');
 
 /**
  * @route POST api/users
