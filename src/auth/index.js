6c854b96336bc80fd74925fd8bf4c289654b65ef
Russ yev <ruslanyev@yahoo.com>
2020-02-16 17:53:45 -0800

    added authentication for logging in

---
+++ src/auth/index.js
@@ -0,0 +1,6 @@
+"use strict";
+
+const { router } = require("./router");
+const { localStrategy, jwtStrategy } = require("./strategies");
+
+module.exports = { router, localStrategy, jwtStrategy };
---
+++ src/auth/router.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const express = require("express");
+const passport = require("passport");
+const jwt = require("jsonwebtoken");
+
+const config = require("../config");
+const router = express.Router();
+
+const createAuthToken = user => {
+  return jwt.sign({ user }, config.JWT_SECRET, {
+    subject: user.name,
+    expiresIn: config.JWT_EXPIRY,
+    algorithm: "HS256"
+  });
+};
+
+const localAuth = passport.authenticate("local", { session: false });
+
+router.post("/login", localAuth, (req, res) => {
+  const user = req.user.serialize();
+  const authToken = createAuthToken(req.user);
+  res.json({ authToken, user });
+});
+
+module.exports = { router };
