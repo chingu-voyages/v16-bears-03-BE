e64402513edc5435f75e2958af159e54bcff7e16
JunjPan <elisepjj@hotmail.com>
2020-02-28 14:37:43 -0800

    add more functions inside the  image component

--- client/src/components/main/sidebar/user/Image.js
+++ client/src/components/main/sidebar/user/Image.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import createJdention from './createJdenticon';
-import { Div, Input, SmallButton, CloseButton } from '../../../../theme/theme';
+import { Div, Input, Label, SmallButton, CloseButton } from '../../../../theme/theme';
 import styled from 'styled-components';
+import axios from 'axios';
 
 const BigProfileImage = styled.div`
   margin: 1rem auto;
@@ -31,31 +32,68 @@ function Image(props) {
     createJdention(url, props.id, 'bigimage');
   }
 
+  function resetImage() {
+    createJdention(props.image, props.id, 'bigimage');
+    setUrl('');
+  }
+
+  //Todo:
+  function deleteImage() {
+    //axios.patch()
+    console.log('delete image');
+  }
+
+  //Todo
+  function updateImage(e) {
+    e.preventDefault();
+    //axios.patch()
+
+    axios
+      .patch(`/api/user/:id`, { userImage: url })
+      .then(res => {
+        console.log(res.data);
+        props.dispatch({ type: 'CHANGE_IMAGEURL', imageurl: url });
+      })
+      .catch(err => console.log(err.response.data));
+  }
+
   return (
     <Div type="image">
       <CloseButton onClick={() => props.SetOpenImageWindow(false)}>X</CloseButton>
+
       <BigProfileImage id="bigimage"></BigProfileImage>
 
-      <form>
-        <Input
-          defaultValue="Please input image url"
-          id="urlinput"
-          onChange={e => setUrl(e.target.value)}
-          autoFocus
-        />
+      <form onSubmit={updateImage}>
+        <Label htmlFor="urlinput">
+          Image URL:
+          <br />
+          <Input
+            type="url"
+            id="urlinput"
+            value={url}
+            title="Please enter a valid image URL."
+            onChange={e => {
+              e.preventDefault();
+              setUrl(e.target.value);
+            }}
+            autoFocus
+          />
+        </Label>
 
         <ButtonDiv>
           <SmallButton type="button" onClick={previewImage}>
             Preview
           </SmallButton>
 
-          <SmallButton type="button">Cancel</SmallButton>
+          <SmallButton type="button" onClick={resetImage}>
+            Reset
+          </SmallButton>
 
           <SmallButton type="submit" background="white">
             Update
           </SmallButton>
 
-          <SmallButton type="button" background="white">
+          <SmallButton type="button" background="white" onClick={deleteImage}>
             Delete
           </SmallButton>
         </ButtonDiv>
