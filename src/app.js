bd4c4e72652469b84193ef1cc33744df7bf5dafe
rusye <42260235+rusye@users.noreply.github.com>
2020-03-04 19:18:19 -0800

    Adding path as requirment

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
 const passport = require('passport');
+const path = require('path');
 
 const { router: authRouter, localStrategy, jwtStrategy } = require('./auth');
 const userRouter = require('./routes/users');
