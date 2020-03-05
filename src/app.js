21b9ef8a1952c7100b53c350509b24c9dba085a9
Russ yev <ruslanyev@yahoo.com>
2020-03-04 18:11:18 -0800

    configured server and client for depolyment

--- src/app.js
+++ src/app.js
@@ -49,4 +49,13 @@ app.use(function errorHandler(error, req, res, next) {
   res.status(500).json(response);
 });
 
+//Serve Static Assets in production
+//set static folder
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 module.exports = app;
